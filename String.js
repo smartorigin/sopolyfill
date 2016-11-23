47168478f22795010b869cc49be6f74526dd6e19
Tony Georges <tony.georges@smart-origin.com>
2016-11-23 11:07:33 +0100

    Initial commit with basic Strings polyfills

---
+++ String.js
@@ -0,0 +1,27 @@
+/**
+ * Created by tony on 23/11/2016.
+ */
+define(function()
+{
+  if (!String.prototype.endsWith) {
+    String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.lastIndexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+    };
+  }
+
+
+  if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+    };
+  }
+  return String;
+
+});
---
+++ main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by tony on 23/11/2016.
+ */
+define(['sopolyfill/String'], function(String)
+{
+  return {};
+});
